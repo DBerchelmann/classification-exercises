13290da0851550a275ff44a9bdd0443ffbaf3cf6
David Berchelmann <david.a.berchelmann@gmail.com>
2021-02-24 07:02:41 -0600

    updated solutions for classification_exercise and updated prepare.py functions

--- prepare.py
+++ prepare.py
@@ -1,10 +1,18 @@
-import acquire
 import pandas as pd
 import numpy as np
+import os
+
+### from acquire.py
+
 from env import host, user, password
 from pydataset import data
+from acquire import get_connection, new_iris_data, get_iris_data
 
-iris_prac = acquire.get_iris_data()
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+
+
+### Clean the Data ###
 
 def clean_iris():
     '''
@@ -14,13 +22,45 @@ def clean_iris():
     table will be concatanted to bring it all together as on dataframe
     
     return: single cleaned dataframe
+
     '''
     
-    iris_prac.drop(['species_id', 'measurement_id'], inplace=True)
-    iris_prac.rename(columns={"species_name": "species"}, inplace=True)
-    dummies = pd.get_dummies(iris_prac[['species']], drop_first=False)
-    pd.get_dummies(iris_prac[['species']], drop_first=False)
-    return pd.concat([iris_prac, dummies], axis=1)
+    df = get_iris_data()
+    df['species'] = df.species_name
+    dropcols = ['species_id', 'measurement_id', 'species_name']
+    df.drop(columns=dropcols, inplace=True)
+    dummies = pd.get_dummies(df[['species']], drop_first=False)
+    pd.get_dummies(df[['species']], drop_first=False)
+    return pd.concat([df, dummies], axis=1)
+
+
+### Train, Test, Validate
+
+
+df = clean_iris()
+train, test = train_test_split(df, test_size=.2, random_state=123, stratify=df.species)
+train, validate = train_test_split(train, test_size=.3, random_state=123, stratify=train.species)
+def impute_mode():
+    '''
+    impute mode for species
+    '''
+    imputer = SimpleImputer(strategy='most_frequent')
+    train[['species']] = imputer.fit_transform(train[['species']])
+    validate[['species']] = imputer.transform(validate[['species']])
+    test[['species']] = imputer.transform(test[['species']])
+    return train, validate, test
+
+def prep_iris_data():
+    df = clean_iris()
+    train_validate, test = train_test_split(df, test_size=.2, random_state=123, stratify=df.species)
+    train, validate = train_test_split(train_validate, 
+                                       test_size=.3, 
+                                       random_state=123, 
+                                       stratify=train_validate.species)
+    train, validate, test = impute_mode()
+    return train, validate, test
+
+  
 
 
 
