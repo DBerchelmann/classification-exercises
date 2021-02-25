05eb1e916597fd7dad8a3149c9fec2dcdcef3121
David Berchelmann <david.a.berchelmann@gmail.com>
2021-02-25 16:06:53 -0600

    added split function

--- prepare.py
+++ prepare.py
@@ -64,4 +64,17 @@ def prep_iris_data():
   
 
 
-
+def split(df, stratify_by=None):
+    """
+    Crude train, validate, test split
+    To stratify, send in a column name
+    """
+    
+    if stratify_by == None:
+        train, test = train_test_split(df, test_size=.3, random_state=123)
+        train, validate = train_test_split(df, test_size=.3, random_state=123)
+    else:
+        train, test = train_test_split(df, test_size=.2, random_state=123, stratify=df[stratify_by])
+        train, validate = train_test_split(df, test_size=.3, random_state=123, stratify=train[stratify_by])
+    
+    return train, validate, test
